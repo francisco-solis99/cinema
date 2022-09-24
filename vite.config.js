bb5a0a3d457167b6855e6d87faaf9ef70b3e9b31
francisco-solis99 <franciscosoliscss@gmail.com>
2022-09-23 21:15:05 -0500

    [Add] the sass partials

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const base = mode === 'production' ? '/' + path.basename(process.cwd()) + '/' : '/';
+
+export default defineConfig({
+  root: 'src',
+  base,
+  mode,
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    assetsDir: './'
+  }
+});
