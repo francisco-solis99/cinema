c47dd18b3c09b5b08f60905d5d5ddae456a8164f
francisco-solis99 <franciscosoliscss@gmail.com>
2022-09-28 18:45:09 -0500

    [Style] the hover and scaleof  movie card

--- src/main.js
+++ src/main.js
@@ -9,23 +9,22 @@ function getDataInJson(url) {
     .then(response => response.json())
     .catch(err => console.log(err));
 }
-
-async function getTrendingMovies() {
-  const urlTendencies = `${API.base}${API.trending}/movie/week?api_key=${API.key}`;
-  const { results: trendingMovies } = await getDataInJson(urlTendencies);
-  return trendingMovies;
+async function getMovies(urlName) {
+  const urlMovies = getUrl(urlName);
+  const { results: moviesList } = await getDataInJson(urlMovies);
+  return moviesList;
 }
 
 async function renderBestTrendingMovie() {
   const header = document.querySelector('.header');
-  const [bestTrendingMovie] = await getTrendingMovies();
+  const [bestTrendingMovie] = await getMovies('trendingMovies');
   header.style.backgroundImage = `linear-gradient(to bottom, rgba(45, 41, 64, 0.45), rgba(13, 23, 53, 1)), url('https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${bestTrendingMovie.backdrop_path}')`;
   header.querySelector('.best__movie-title').textContent = bestTrendingMovie.title;
 }
 
-async function renderMovies({ htmlSelectorSection, callbackGetMovies }) {
+async function renderMovies({ htmlSelectorSection, callbackGetMovies, urlName }) {
   const section = document.querySelector(htmlSelectorSection);
-  const tendenciesMovies = await callbackGetMovies();
+  const tendenciesMovies = await callbackGetMovies(urlName);
   const fragment = document.createDocumentFragment();
 
   tendenciesMovies.forEach(movie => {
@@ -50,5 +49,15 @@ function createMovieCard(movie) {
   return card;
 }
 
+function getUrl(label) {
+  const urlsApi = {
+    trendingMovies: `${API.base}${API.trending}/movie/week?api_key=${API.key}`,
+    upcomingMovies: `${API.base}/movie/upcoming?api_key=${API.key}`,
+    nowPlayingMovies: `${API.base}/movie/now_playing?api_key=${API.key}`
+  };
+  return urlsApi[label];
+}
 renderBestTrendingMovie();
-renderMovies({ htmlSelectorSection: '.section__tendencies .movies__cards', callbackGetMovies: getTrendingMovies });
+renderMovies({ htmlSelectorSection: '.section__tendencies .movies__cards', callbackGetMovies: getMovies, urlName: 'trendingMovies' });
+renderMovies({ htmlSelectorSection: '.section__play-now .movies__cards', callbackGetMovies: getMovies, urlName: 'nowPlayingMovies' });
+renderMovies({ htmlSelectorSection: '.section__upcoming .movies__cards', callbackGetMovies: getMovies, urlName: 'upcomingMovies' });
