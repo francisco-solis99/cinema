9fb864a36b123a1ae375d386d03532cac9417eb8
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-07 17:12:58 -0600

    [Add] push history state in DOMContentLoaded

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import { Router } from './router/router.js';
 
 const router = new Router();
 
-window.addEventListener('DOMContentLoaded', () => router.init(), false);
+window.addEventListener('DOMContentLoaded', () => {
+  router.init();
+  window.history.pushState({ backUrl: window.location.href }, null, '');
+}, false);
 window.addEventListener('hashchange', () => {
   router.loadRoute();
 }, false);
