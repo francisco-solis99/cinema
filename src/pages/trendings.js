f37fd40c54985daaf1efb6927b372b9e685ed549
francisco-solis99 <franciscosoliscss@gmail.com>
2022-11-07 17:40:50 -0600

    [Refactor] the template html for the movies views

--- src/pages/trendings.js
+++ src/pages/trendings.js
@@ -2,9 +2,9 @@ import { createMovieCard, renderListResults, addBackButton } from '../js/dom.js'
 import { render } from '../js/utils.js';
 
 export default async function() {
-  render('#trendings__view', () => {
+  render('#movies-list__view', () => {
     renderListResults({
-      htmlSelectorSection: '.trending__movies',
+      htmlSelectorSection: '.movies__list',
       callbackRender: createMovieCard,
       urlInfo: 'trendingMovies'
     });
