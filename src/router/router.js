6e1e958e8c8620f33bf546b0282d3d7c7355ff00
francisco-solis99 <franciscosoliscss@gmail.com>
2024-12-09 18:46:18 -0600

    [chore] add comments to the router logic file

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import { getURLHash, replaceHTML } from '../js/utils.js';
 import { routes } from './routes.js';
 
+// Router prototype
 export function Router() {
   this.routes = routes;
   this.isFirstLoad = true;
@@ -9,23 +10,30 @@ export function Router() {
 
 Router.prototype = {
   constructor: Router,
+  // Method to init the router
   init() {
+    // Get the initial content
     this.initialContent = document.body.innerHTML;
     this.loadRoute();
   },
 
   async loadRoute() {
+    // Load a path according to the hash of the url
     const routeName = getURLHash();
+    // Render the home
     if (routeName === '') {
       if (!this.isFirstLoad) {
+        // If home is not the first view loaded replace the current body for the intiial content
         replaceHTML(document.body, this.initialContent);
       }
+
       const home = this.routes[0];
       home.view();
       this.isFirstLoad = false;
       window.scrollTo({ top: 0 });
       return;
     }
+    // If get this point, is because a different route to Home was found and it has to render it
     const routeFinded = this.routes.find(route => routeName.startsWith(route.hashStart) && route.hashStart !== '');
     routeFinded.view();
     window.scrollTo({ top: 0 });
