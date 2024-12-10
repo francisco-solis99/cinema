f3059457384111a0124c1decf811dbc89e744b3d
francisco-solis99 <franciscosoliscss@gmail.com>
2024-12-09 18:48:07 -0600

    [chore] add comments to the js files

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,7 @@
 import APIKEY from './key.js';
 import axios from 'axios';
 
+// Config API
 export const API = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import { Router } from './router/router.js';
 
 const router = new Router();
 
+// Init the router object to load the routes of the app
 window.addEventListener('DOMContentLoaded', () => {
   router.init();
   window.history.pushState({ backUrl: window.location.href }, null, '');
 }, false);
+
+// When the has change, load the corresponding path and render the view for that path
 window.addEventListener('hashchange', () => {
   router.loadRoute();
 }, false);
